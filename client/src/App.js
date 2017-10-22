5ad2b5aca808e774258a8d9c0c50682b2386b914
Dan Laush <dan.laush@gmail.com>
2017-10-22 11:03:46 +1100

    Init express app & react app

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  state = {users: []}
+
+  componentDidMount() {
+    // why fetch in cDM?
+    // https://daveceddia.com/where-fetch-data-componentwillmount-vs-componentdidmount/
+    fetch('/users')
+      .then(res => res.json())
+      .then(users => this.setState({ users }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <h2>Users</h2>
+        {this.state.users.map(user =>
+          <div key={user.id}>{user.username}</div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
